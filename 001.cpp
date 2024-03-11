64ca393a983ce60046fc7b98ed6be4ac46d79a72
young <young20050727@gmail.com>
2024-03-11 23:20:06 +0800

    improve 001.cpp

--- 001.cpp
+++ 001.cpp
@@ -1,41 +1,18 @@
-#include <iostream>
 #include <vector>
-using namespace std;
-
-void twoSum(vector<int> nums, int target){
-    int a = 0;
-    int b = 0;
-    for (int i=0;i<nums.size();i++){
-        for (int j=nums.size();j>=i;j--){
-            a=nums[i];
-            b=nums[j];
-            if (a+b == target){
-                cout << "[" << i << "," << j << "]";
-            }
-        }
-    }
-}
-
-int main() {
-    vector<int> v = {2,7,11,15,37,1,23};
-    int target = 26;
-    twoSum(v,target);
-    return 0;
-} 
 
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> array;
+        std::vector<int> vec;
         for (int i=0;i<nums.size();i++){
             for (int j=nums.size()-1;j>i;j--){
                 if (nums[i]+nums[j] == target){
-                    array.push_back(i);
-                    array.push_back(j);
-                    return array;
+                    vec = {i, j};
+                    return vec;
                 }
             }
         }
-        return array;
+
+        return vec;
     }
 };
