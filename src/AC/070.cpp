a40ceefde47a3bf5598348b12d59caf7c7d47812
young <young20050727@gmail.com>
2024-04-12 07:00:49 +0800

    070.cpp

---
+++ src/AC/070.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        std::vector<int> stair_solution_n = {1, 2};
+        stair_solution_n.resize(n);
+        for (int i=2; i<n; i++) {
+            stair_solution_n[i] = stair_solution_n[i-1] + stair_solution_n[i-2];
+        }
+
+        return stair_solution_n[n-1];
+    }
+};
