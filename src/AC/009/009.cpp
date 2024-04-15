a3576ae8f00d94d96df1492e42655b8bb1463aa3
young <young20050727@gmail.com>
2024-04-15 23:56:17 +0800

    009.cpp 050.cpp

--- src/AC/009/009.cpp
+++ src/AC/009/009.cpp
@@ -4,14 +4,15 @@ public:
         if(x<0){
             return false;
         }
+
         long long int reversed_num = 0, remainder, reg;
         reg = x;
         while(x != 0) {
             remainder = x % 10;
             reversed_num = reversed_num * 10 + remainder;
             x /= 10;
-            std::cout << reversed_num << std::endl;
         }
+
         return reg == reversed_num;
     }
 };
