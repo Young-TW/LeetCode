9c33859e097f4909b363db0beff5c2b9d8c66d63
young <young20050727@gmail.com>
2024-04-28 09:38:14 +0800

    014.cpp

---
+++ src/AC/014/014.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+class Solution {
+public:
+    std::string longestCommonPrefix(std::vector<std::string>& strs) {
+        std::string prefix = strs[0];
+        for (int i = 1; i < strs.size(); i++) {
+            int j = 0;
+            while (j < prefix.size() && j < strs[i].size() && prefix[j] == strs[i][j]) {
+                j++;
+            }
+            prefix = prefix.substr(0, j);
+        }
+
+        return prefix;
+    }
+};
