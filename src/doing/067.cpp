93dc4625e9869b601d4452f5ede33d7bec8f4147
young <young20050727@gmail.com>
2024-07-10 05:38:09 +0800

    update .gitignore

--- src/doing/067.cpp
+++ src/doing/067.cpp
@@ -7,7 +7,6 @@ public:
     std::string addBinary(std::string a, std::string b) {
         if (a.length() != b.length()) {
             int length = std::max(a.length(), b.length());
-            
         }
     }
 };
