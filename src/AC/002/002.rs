378a591307cba9a54a23561dc8f04d85a9f114a8
young <young20050727@gmail.com>
2024-07-19 11:25:59 +0800

    002.rs

---
+++ src/AC/002/002.rs
@@ -0,0 +1,41 @@
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+//
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+
+impl Solution {
+    pub fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut ans_head = None;
+        let mut ans = &mut ans_head;
+        let mut carry = 0;
+        let (mut l1, mut l2) = (l1, l2);
+        while l1.is_some() || l2.is_some() || carry != 0 {
+            let mut sum = carry;
+            if let Some(node) = l1 {
+                sum += node.val;
+                l1 = node.next;
+            }
+            if let Some(node) = l2 {
+                sum += node.val;
+                l2 = node.next;
+            }
+            carry = sum / 10;
+            *ans = Some(Box::new(ListNode::new(sum % 10)));
+            ans = &mut ans.as_mut().unwrap().next;
+        }
+
+        ans_head
+    }
+}
