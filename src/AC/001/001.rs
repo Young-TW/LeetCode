7cc7ecaeae98529b69e3b5ddc883ede9da99b590
Young <young20050727@gmail.com>
2024-11-16 12:29:01 +0800

    improve 001.rs

--- src/AC/001/001.rs
+++ src/AC/001/001.rs
@@ -1,11 +1,17 @@
+use std::collections::HashMap;
+
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        for i in 0..nums.len() {
-            for j in i + 1..nums.len() {
-                if nums[i] + nums[j] == target {
-                    return vec![i as i32, j as i32];
-                }
+        let mut map = HashMap::new();
+
+        for (i, &num) in nums.iter().enumerate() {
+            let complement = target - num;
+
+            if let Some(&index) = map.get(&complement) {
+                return vec![index as i32, i as i32];
             }
+
+            map.insert(num, i);
         }
 
         vec![]
